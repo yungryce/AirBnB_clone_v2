669cf49f425d1c06d7b506009c1cc15c4891a703
chxgbx <chigbujoshua@yahoo.com>
2024-03-06 13:26:59 +0000

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,6 +9,7 @@ from os.path import exists
 
 env.hosts = ['52.201.190.114', '52.86.172.121']
 
+
 def do_deploy(archive_path):
     """Distributes an archive to a web server.
     Args:
@@ -27,16 +28,15 @@ def do_deploy(archive_path):
         put(archive_path, "/tmp/{}".format(file))
         run("rm -rf /data/web_static/releases/{}/".format(name))
         run("mkdir -p /data/web_static/releases/{}/".format(name))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".\
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
             format(file, name))
         run("rm /tmp/{}".format(file))
         run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name))
-        run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name))
+            "/data/web_static/releases/{}/".format(name, name))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(name))
         run("rm -rf /data/web_static/current")
         run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name))
+            format(name))
         return True
-    except:
+    except Exception:
         return False
