7504c40027a68ddee7c679fb425a94120bdb7ce4
chxgbx <chigbujoshua@yahoo.com>
2024-03-24 21:51:15 +0100

    List of states

--- models/place.py
+++ models/place.py
@@ -8,14 +8,12 @@ from sqlalchemy.orm import relationship
 if models.type_storage == 'db':
     place_amenity = Table('place_amenity', Base.metadata,
                           Column('place_id', String(60),
-                                 ForeignKey('places.id', onupdate='CASCADE',
-                                            ondelete='CASCADE'),
-                                 primary_key=True),
-                          Column('amenity_id', String(60),
-                                 ForeignKey('amenities.id', onupdate='CASCADE',
-                                            ondelete='CASCADE'),
-                                 primary_key=True))
-
+                                 ForeignKey('places.id'),
+                                 primary_key=True, nullable=False),
+                          Column('amenity_id',
+                                 String(60),
+                                 ForeignKey('amenities.id'),
+                                 primary_key=True, nullable=False))
 
 class Place(BaseModel, Base):
     """Representation of Place """
