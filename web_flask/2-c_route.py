41e09f6f4807660a64a4417be161144415840f76
chxgbx <chigbujoshua@yahoo.com>
2024-03-24 08:09:24 +0100

    C is fun!

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -18,10 +18,12 @@ def hbnb():
     """returns HBNB"""
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
     """display “C ” followed by the value of the text variable"""
     return 'C ' + text.replace('_', ' ')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
